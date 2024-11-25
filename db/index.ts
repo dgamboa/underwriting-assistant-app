9b9e8ce0fa5f7a0ecb7ef92cf2a5bcd4e687f5a6
Daniel Gamboa <danielgamboa10@gmail.com>
2024-11-25 14:10:44 -0600

    Add boiler plate db schema

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { factsTable } from "./schema/facts-schema";
+import { documentsTable } from "./schema/documents-schema";
 
 config({ path: ".env.local" });
 
