7f36a3773409505bcbbffe71eeafae0bf6bc0fb0
Daniel Gamboa <danielgamboa10@gmail.com>
2024-12-09 12:38:37 -0600

    Fix another bug on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <main className="container mx-auto p-4 h-screen">
       <div className="flex flex-col-reverse md:flex-row h-full">
-        <div className="flex-1 md:pr-4 mt-4 md:mt-0 h-[50vh] md:h-full">
+        <div className="flex-1 md:pr-4 mt-4 md:mt-0 md:h-full">
           <DocumentUploader />
         </div>
 
-        <div className="h-[50vh] md:h-full flex-1 md:pl-4">
+        <div className="md:h-full flex-1 md:pl-4">
           <AiChat />
         </div>
       </div>
