252f26cb404c7ee0e24ee00c0b2474110ce10906
Daniel Gamboa <danielgamboa10@gmail.com>
2024-12-09 12:34:10 -0600

    Fix mobile view bug on screen size

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import DocumentUploader from "@/components/document-uploader";
 export default function Home() {
   return (
     <main className="container mx-auto p-4 h-screen">
-      <div className="flex flex-col-reverse md:flex-row h-full relative">
-        <div className="absolute top-[100vh] w-full h-full md:static md:h-full flex-1 md:pr-4">
+      <div className="flex flex-col-reverse md:flex-row h-full">
+        <div className="flex-1 md:pr-4 mt-4 md:mt-0 h-[50vh] md:h-full">
           <DocumentUploader />
         </div>
 
-        <div className="h-full flex-1 md:pl-4">
+        <div className="h-[50vh] md:h-full flex-1 md:pl-4">
           <AiChat />
         </div>
       </div>
